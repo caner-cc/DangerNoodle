59b60f6257dce72087ff63c2cc5e30598ccf5e48
Lirppis <60780018+Lirppis@users.noreply.github.com>
2021-02-11 19:02:49 +0200

    Update App.js
    Testing for conflicts

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-<text>Caner wrote this thingy </text>
+<text>Mikael wrote this thingy </text>
 <text>And i also updated this thingy</text>
 <text>Hey here's another update </text>
+<text>Testing conflict </text>
 <text>Recipe</text>
 <text>And i also updated this thingy</text>
 
